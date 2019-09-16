d0d88dd3c50ea40d7d3da1d9c7e6368848a2d8b2
Tom Lynch <tomglynch@gmail.com>
2019-09-16 21:29:18 +1000

    SQL fixed and it added our heroku DB datasource automatically to dataSource.xml so see if that auto does it for you

--- src/main/java/db/DB_setup.sql
+++ src/main/java/db/DB_setup.sql
@@ -4,8 +4,11 @@ DROP TABLE DCsRetailers;
 DROP TABLE Transactions;
 DROP TABLE Inventory;
 
-CREATE TYPE flavour AS ENUM (''regular'', ''vanilla'', ''zero'');
-CREATE TYPE colaForm AS ENUM (''syrup'', ''pallet'');
+DROP TYPE flavour;
+DROP TYPE colaForm;
+
+CREATE TYPE flavour AS ENUM ('regular', 'vanilla', 'zero');
+CREATE TYPE colaForm AS ENUM ('syrup', 'pallet');
 
 CREATE TABLE DCs(
 	DCId SERIAL PRIMARY KEY,
@@ -52,24 +55,24 @@ INSERT INTO DCs(
 	name
 )
 VALUES
-(''Melbourne DC''),
-(''Sydney DC''),
-(''Brisbane DC''),
-(''Perth DC'');
+('Melbourne DC'),
+('Sydney DC'),
+('Brisbane DC'),
+('Perth DC');
 
 INSERT INTO Retailers(
-	name
+    name
 )
 VALUES
-(''Coles Collingwood''),
-(''Coles Fitzroy''),
-(''Coles Sydney''),
-(''Coles Perth'');
+('Coles Collingwood'),
+('Coles Fitzroy'),
+('Coles Sydney'),
+('Coles Perth');
 
 
 INSERT INTO DCsRetailers(
-	DCId,
-	RetailerId
+    DCId,
+    RetailerId
 )
 VALUES
 (1,1),
@@ -79,40 +82,40 @@ VALUES
 
 
 INSERT INTO Transactions(
-	price,
-	inventoryID,
-	txTime,
-	sellerID,
-	buyerID
+    price,
+    inventoryID,
+    txTime,
+    sellerID,
+    buyerID
 )
 VALUES
-(''$220'', 1, CURRENT_TIMESTAMP, 1, 2),
-(''$333'', 2, CURRENT_TIMESTAMP, 2, 3),
-(''$102'', 3, CURRENT_TIMESTAMP, 1, 2),
-(''$50.25'', 4, CURRENT_TIMESTAMP, 4, 4),
-(''$253'', 5, CURRENT_TIMESTAMP, 1, 1),
-(''$132'', 6, CURRENT_TIMESTAMP, 2, 3),
-(''$425'', 7, CURRENT_TIMESTAMP, 1, 2),
-(''$80.5'', 8, CURRENT_TIMESTAMP, 1, 2);
+('$220', 1, CURRENT_TIMESTAMP, 1, 2),
+('$333', 2, CURRENT_TIMESTAMP, 2, 3),
+('$102', 3, CURRENT_TIMESTAMP, 1, 2),
+('$50.25', 4, CURRENT_TIMESTAMP, 4, 4),
+('$253', 5, CURRENT_TIMESTAMP, 1, 1),
+('$132', 6, CURRENT_TIMESTAMP, 2, 3),
+('$425', 7, CURRENT_TIMESTAMP, 1, 2),
+('$80.5', 8, CURRENT_TIMESTAMP, 1, 2);
 
 
 INSERT INTO Inventory(
-	DCId,
-	flavourType,
-	sold
+    DCId,
+    flavourType,
+    sold
 )
 VALUES
-(1,''regular'',true),
-(2,''vanilla'',true),
-(3,''regular'',true),
-(4,''zero'',true),
-(5,''vanilla'',true),
-(6,''regular'',true),
-(7,''zero'',true),
-(8,''regular'',true),
-(9,''regular'',false),
-(10,''zero'',false),
-(11,''vanilla'',false),
-(12,''regular'',false),
-(13,''regular'',false),
-(14,''vanilla'',false);
+(1,'regular',true),
+(2,'vanilla',true),
+(3,'regular',true),
+(4,'zero',true),
+(5,'vanilla',true),
+(6,'regular',true),
+(7,'zero',true),
+(8,'regular',true),
+(9,'regular',false),
+(10,'zero',false),
+(11,'vanilla',false),
+(12,'regular',false),
+(13,'regular',false),
+(14,'vanilla',false);
